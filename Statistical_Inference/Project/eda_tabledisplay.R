9d0494318d2441db17bd632b808e2015064ee1ef
Andrew Doyle <doylead@Andrews-Air.attlocal.net>
2018-04-16 21:51:24 -0700

    doylead: added summary table graphic

--- Statistical_Inference/Project/eda_tabledisplay.R
+++ Statistical_Inference/Project/eda_tabledisplay.R
@@ -1,20 +1,31 @@
+setwd('~/Dropbox/Coursera/JHU_DataScience/Statistical_Inference/Project')
+
 library(grid)
 library(gridExtra)
+
+tt <- ttheme_minimal(
+  core=list(bg_params = list(fill = blues9[2:4], col=NA),
+  fg_params=list(fontface=3)),
+  colhead=list(fg_params=list(col="navyblue", fontface=4L), bg_params=list(col=blues9[1])))
 
 data(ToothGrowth)
 df <- ToothGrowth
 
-s <- aggregate(len~supp+dose,df,sd)
-t <- aggregate(len~supp+dose,df,mean)
-s <- s[with(s,order(supp)),]
-t <- t[with(t,order(supp)),]
-names(s) <- c("Supplement","Dose (mg/day)","Standard Deviation\n of Length (Unknown Units)")
-names(t) <- c("Supplement","Dose (mg/day)","Mean Length (Unknown Units)")
+df.c <- aggregate(len~supp+dose,df,length)
+df.m <- aggregate(len~supp+dose,df,mean)
+df.s <- aggregate(len~supp+dose,df,sd)
 
-png("mean_table.png")
-grid.table(t,rows=NULL)
-dev.off()
+df.cm <- merge(df.c,df.m,by=c("supp","dose"))
+df <- merge(df.cm,df.s,by=c("supp","dose"))
 
-png("std_table.png")
-grid.table(s,rows=NULL)
-dev.off()
+names(df) <- c("Supplement","Dose\n (mg/day)","Sample\n Size","Mean Length\n (Unknown Units)","Standard Deviation\n of Length\n (Unknown Units)")
+
+df <- df[with(df,order(Supplement)),]
+df$`Standard Deviation
+ of Length
+ (Unknown Units)` <- round(df$`Standard Deviation
+ of Length
+ (Unknown Units)`,2)
+
+grid.table(df,rows=NULL,theme=tt)
+# Export at 590,250
